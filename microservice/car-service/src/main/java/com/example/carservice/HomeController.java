1c4b2eeaddb27f86483871a468c8a8fbeaa1ab32
Jeferson Cruz <dev.blackfishlabs@gmail.com>
2019-09-06 10:01:59 -0400

    Build Basic Arch Microservices

---
+++ microservice/car-service/src/main/java/com/example/carservice/HomeController.java
@@ -0,0 +1,19 @@
+package com.example.carservice;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.security.Principal;
+
+@RestController
+public class HomeController {
+
+    private final static Logger log = LoggerFactory.getLogger(HomeController.class);
+
+    @GetMapping("/home")
+    public String howdy() {
+        return "Hello, Jeferson!";
+    }
+}
